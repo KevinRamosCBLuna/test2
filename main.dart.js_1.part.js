203a888564af69378889f2922d80d432d3d93d72
Kevin Ramos <kevin.ramos@cbluna.com>
2023-07-05 10:34:28 -0700

    .

---
+++ main.dart.js_1.part.js
@@ -0,0 +1,9 @@
+self.$__dart_deferred_initializers__=self.$__dart_deferred_initializers__||Object.create(null)
+$__dart_deferred_initializers__.current=function(a,b,c,$){var A={R8:function R8(){}},B
+A=a.updateHolder(c[3],A)
+B=c[0]
+A.R8.prototype={
+v0(d){return"Hej "+d+"!"}}
+var z=a.updateTypes([]);(function inheritance(){var y=a.inherit
+y(A.R8,B.B)})()}
+$__dart_deferred_initializers__["UeOROqeIs3jmy3Yd/v6WrZYAFao="] = $__dart_deferred_initializers__.current
---
+++ main.dart.js_3.part.js
@@ -0,0 +1,9 @@
+self.$__dart_deferred_initializers__=self.$__dart_deferred_initializers__||Object.create(null)
+$__dart_deferred_initializers__.current=function(a,b,c,$){var A={Kw:function Kw(){}},B
+A=a.updateHolder(c[4],A)
+B=c[0]
+A.Kw.prototype={
+v0(d){return"Hello "+d+"!"}}
+var z=a.updateTypes([]);(function inheritance(){var y=a.inherit
+y(A.Kw,B.B)})()}
+$__dart_deferred_initializers__["t3z2hgzNqIRFL7wt6Xn2U2/inaw="] = $__dart_deferred_initializers__.current
